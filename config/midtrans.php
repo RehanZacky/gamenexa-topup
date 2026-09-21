eea3c51ee6333c4cc5f59390a40e66f4609d8725
RehanZacky <rehanzacky.rz@gmail.com>
2026-09-21 10:45:49 +0700

    tes

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'server_key' => env('MIDTRANS_SERVER_KEY', ''),
-    'client_key' => env('MIDTRANS_CLIENT_KEY', ''),
-    'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
+    'merchant_id' => env('MIDTRANS_MERCHANT_ID', 'M871410395'),
+    'server_key' => env('MIDTRANS_SERVER_KEY', 'Mid-server-HuitNBRFTN8FrQe1O2YvZ0Kd'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY', 'Mid-client-8DfzmeIT1K6XjBwF'),
+    'is_production' => env('MIDTRANS_IS_PRODUCTION', true),
     'is_sanitized' => env('MIDTRANS_IS_SANITIZED', true),
     'is_3ds' => env('MIDTRANS_IS_3DS', true),
-    'snap_url' => env('MIDTRANS_IS_PRODUCTION', false)
+    'snap_url' => env('MIDTRANS_IS_PRODUCTION', true)
         ? 'https://app.midtrans.com/snap/snap.js'
         : 'https://app.sandbox.midtrans.com/snap/snap.js',
 ];
